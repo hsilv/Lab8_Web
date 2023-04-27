b24a4d268973cb5363981081e486c63609570f28
hsilv <hm.sebastiansilva@gmail.com>
2023-04-27 16:04:55 -0600

    Inclusion de logica de memoria

--- src/App.jsx
+++ src/App.jsx
@@ -32,22 +32,85 @@ const cardImg = [
 export function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+  const [finished, setFinished] = useState(false);
 
   const shuffleCards = () => {
     const shuffledCards = [...cardImg, ...cardImg]
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }));
 
+    setChoiceOne(null);
+    setChoiceTwo(null);
     setCards(shuffledCards);
     setTurns(0);
   };
+
+  const handleCard = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+    setDisabled(false);
+  };
+
+  useEffect(() => {
+    shuffleCards();
+  }, []);
+
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true);
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1000);
+      }
+    }
+    if(cards.every((card) => card.matched === true)){
+        setFinished(true);
+        console.log('Finished');
+    }else{
+        setFinished(false);
+    }
+  }, [choiceOne, choiceTwo, cards]);
 
   return (
     <div>
       <div className="gameHeader">
         <h1>Juego de Memoria</h1>
         <Button title="Iniciar partida" onClick={shuffleCards}></Button>
+        <h3 className="turns">Turnos: {turns}</h3>
+        <h3 className={finished ? "finished": "fade"}>Felicitaciones, ha completado la memoria! 🤙</h3>
+      </div>
+      <div className="table">
+        {cards.map((card) => (
+          <Card
+            disabled={disabled}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            key={card.id}
+            card={card}
+            front={card.src}
+            back={img11}
+            handleChoice={handleCard}
+          ></Card>
+        ))}
       </div>
     </div>
   );
 }
+
