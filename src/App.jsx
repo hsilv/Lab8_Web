4a7ff53ed62b6b6b79affd9e122cb48fde57e264
hsilv <hm.sebastiansilva@gmail.com>
2023-04-27 15:55:42 -0600

    Creación de App

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+
+export function App() {
+  return (
+    <div>
+      <div className="gameHeader">
+        <h1>Juego de Memoria</h1>
+      </div>
+    </div>
+  );
+}
