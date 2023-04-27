6f2a9aaddfffe9145ba9eb8d7ed6482659ebbb73
hsilv <hm.sebastiansilva@gmail.com>
2023-04-27 16:01:19 -0600

    Inclusion de imagenes para tarjetas

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,31 @@ import ReactDOM from "react-dom/client";
 import "./styles/global.scss";
 import { Button } from "./components/Button";
 
+import img1 from "./img/1.png";
+import img2 from "./img/2.png";
+import img3 from "./img/3.png";
+import img4 from "./img/4.png";
+import img5 from "./img/5.png";
+import img6 from "./img/6.png";
+import img7 from "./img/7.png";
+import img8 from "./img/8.png";
+import img9 from "./img/9.png";
+import img10 from "./img/10.png";
+import img11 from "./img/11.png";
+
+const cardImg = [
+  { src: img1, matched: false },
+  { src: img2, matched: false },
+  { src: img3, matched: false },
+  { src: img4, matched: false },
+  { src: img5, matched: false },
+  { src: img6, matched: false },
+  { src: img7, matched: false },
+  { src: img8, matched: false },
+  { src: img9, matched: false },
+  { src: img10, matched: false },
+];
+
 export function App() {
   return (
     <div>
