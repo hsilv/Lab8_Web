c7c1e19e5962697ddd638b2d7f4f4376e8605b83
hsilv <hm.sebastiansilva@gmail.com>
2023-04-27 15:59:46 -0600

    Inclusion de boton a App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/global.scss";
+import { Button } from "./components/Button";
 
 export function App() {
   return (
     <div>
       <div className="gameHeader">
         <h1>Juego de Memoria</h1>
+        <Button title="Iniciar partida"></Button>
       </div>
     </div>
   );
